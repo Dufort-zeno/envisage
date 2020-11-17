258ba242a72d056f641ffed030b927d162bc024c
Anunayj <anunay.j1@gmail.com>
2020-11-18 00:48:02 +0530

    Basically created everything

--- src/components/home.js
+++ src/components/home.js
@@ -1,49 +1,180 @@
+import React from 'react'; 
 import logo from '../logo.svg';
 import tick from './assets/tick.png'
-import './scripts.js';
+// import {updatePermission} from './scripts.js';
 import '../App.css';
 import { Link } from 'react-router-dom';
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div className="sideflex">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            APPNAME
-          </p>
-        </div>
-        {/* <p>
-          Don't Edit <code>src/components/home.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Don't Learn React
-        </a>
-        <Link to="/about">About us</Link>
-         */}
-      <div className="center">
-        <div className="done">
-            <img src={tick} alt="Done" className="done"/>
-            <p>
-              You are subscribed to the Warning system, Have a great day!
-            </p>
-        </div>
-        <div className="permissions">
-          <p>Heyooo, looks like we're missing a few permissions! </p>
-             <button id="location-btn">Location</button>
-             <button id="notification-btn" onClick={()=>{console.log("hi")}}>Notification</button>
-        </div>
-      </div>
-      </header>
-      
+
+function Done(){
+  return(
+    <div className="done">
+             <img src={tick} alt="Done" className="done"/>
+             <p>
+               You are subscribed to the Warning system, Have a great day!
+             </p>
     </div>
+  )
+}
+
+function Failed(){
+  return(
+    <p>
+      FAILED!
+    </p>
+
+
+  )
+}
+
+class Permissions extends React.Component{
+  constructor(props){
+    super(props);
+    console.log(props)
+  }
+  notificationHandler(){
+    Notification.requestPermission();
+  }
+  locationHandler(){
+    navigator.geolocation.getCurrentPosition((location)=>{
+      localStorage.setItem('location', location);
+      this.props.update();
+
+    })
+  }
+
+  render(){
+    let getColor = (result)=>{
+      console.log(result)
+      if(result.state === "granted")
+        return "green";
+      else if(result.state === "denied")
+        return "red";
+      else
+        return null;
+    }
+
+
+    return(
+      <div className="permissions">
+          <p>Heyooo, looks like we're missing a few permissions! </p>
+            <button id="location-btn" onClick={this.locationHandler.bind(this)} style={{backgroundColor: localStorage.getItem('location') ? "gold" : getColor(this.props.result[0])}}>Location</button>
+            <button id="notification-btn" onClick={this.notificationHandler.bind(this)} style={{backgroundColor: getColor(this.props.result[1])}}>Notification</button>
+        </div>
+    )
+  }
+}
+class Subscribed extends React.Component{
+  constructor(props){
+   super(props)
+   this.state = {
+      data: null
+   }
+  }
+  
+  render(){
+    if(this.state.data){
+      return(this.state.data);
+    }else{
+      return(null);
+    }
+  } 
+  async register(){
+    console.log("hello2");
+    function urlB64ToUint8Array(base64String) {
+      const padding = '='.repeat((4 - base64String.length % 4) % 4);
+      const base64 = (base64String + padding)
+        .replace(/\-/g, '+')
+        .replace(/_/g, '/');
+
+      const rawData = window.atob(base64);
+      const outputArray = new Uint8Array(rawData.length);
+
+      for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+      }
+      return outputArray;
+    }
+    if((await navigator.serviceWorker.getRegistrations()).length > 0){
+      let serviceWorkerRegistration = await navigator.serviceWorker.ready;
+      var options = {
+      userVisibleOnly: true,
+      applicationServerKey: urlB64ToUint8Array(
+        "BBg54OqIKc5uCgJFlcDQZQowmem6L1B_wlp7790yOUxHinFEIgV0hh68u_aFLjIKKzACXBbNla_iZP-CtxEA7_I"
+      )
+      };
+      console.log("hello");
+      let pushSubscription = await serviceWorkerRegistration.pushManager.subscribe(options);
+      //TODO: REGISTER WITH SERVER
+      console.log(pushSubscription.endpoint,localStorage.getItem('location'));
+      localStorage.setItem('registered','success')
+      this.updatePermission();
+  }else{
+    localStorage.setItem('registered','failed')
+    this.updatePermission();
+  }
+}
+  updatePermission(){
+    Promise.all([navigator.permissions.query({name:'geolocation'}),
+    navigator.permissions.query({name:'push'}),navigator.permissions.query({name:'notifications'})])
+    .then(function(result) {
+      const forceUpdate = function(){
+        if(this._ismounted)
+          this.updatePermission();
+      }.bind(this)
+
+      result[0].onchange = result[1].onchange = result[2].onchange = forceUpdate;
+      if((result[0].state === 'granted' || localStorage.getItem('location')) && result[1].state === 'granted'){
+        if(localStorage.getItem('registered')==="success")
+          this.setState({data:<Done />});
+        else if(localStorage.getItem('registered')==="failed"){
+          //TODO: Handle Fail and retry
+          this.setState({data:<Failed/>});
+        }
+        else{
+          this.setState({data:null});
+          this.register()
+        }
+      }else{
+        this.setState({data:<Permissions result={result} update={forceUpdate}/>})
+      }
     
-  );
+    }.bind(this))
+  }
+  componentDidMount(){
+    this._ismounted = true;
+    this.updatePermission();
+  }
+  componentWillUnmount(){
+    this._ismounted = false;
+  }
+
+}
+
+
+class App extends React.Component {
+  render(){
+    return (
+        <div className="App">
+          <header className="App-header">
+            <div className="sideflex">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                APPNAME
+              </p>
+            </div>
+            
+          <div className="center">
+           <Subscribed key="0"/>
+          </div>
+          </header>
+          
+        </div>
+        
+      );
+          }
+    componentDidMount(){
+      // updatePermission()
+    }
 }
 
 export default App;
