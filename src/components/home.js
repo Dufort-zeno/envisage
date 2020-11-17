293699d469c7f889b33dd8f188f83d961b2b97c8
Anunayj <anunay.j1@gmail.com>
2020-11-17 15:45:02 +0530

    Finalized topic

--- src/components/home.js
+++ src/components/home.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/components/home.js</code> and save to reload.
+          Don't Edit <code>src/components/home.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Don't Learn React
         </a>
         <Link to="/about">About us</Link>
       </header>
