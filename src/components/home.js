a55e7538612fd77ec2b9b756f33663752d698dd1
Anunayj <anunay.j1@gmail.com>
2020-11-17 20:22:54 +0530

    Added placeholder UI

--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,19 @@
 import logo from '../logo.svg';
+import tick from './assets/tick.png'
+import './scripts.js';
 import '../App.css';
 import { Link } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <div className="sideflex">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            APPNAME
+          </p>
+        </div>
+        {/* <p>
           Don't Edit <code>src/components/home.js</code> and save to reload.
         </p>
         <a
@@ -18,8 +25,24 @@ function App() {
           Don't Learn React
         </a>
         <Link to="/about">About us</Link>
+         */}
+      <div className="center">
+        <div className="done">
+            <img src={tick} alt="Done" className="done"/>
+            <p>
+              You are subscribed to the Warning system, Have a great day!
+            </p>
+        </div>
+        <div className="permissions">
+          <p>Heyooo, looks like we're missing a few permissions! </p>
+             <button id="location-btn">Location</button>
+             <button id="notification-btn" onClick={()=>{console.log("hi")}}>Notification</button>
+        </div>
+      </div>
       </header>
+      
     </div>
+    
   );
 }
 
